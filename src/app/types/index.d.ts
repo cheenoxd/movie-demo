43a999bc70c101ac0e3c39addd4c263aa4f85113
Zesan <90212746+cheenoxd@users.noreply.github.com>
2024-11-26 23:05:19 -0500

    Update index.d.ts

--- src/app/types/index.d.ts
+++ src/app/types/index.d.ts
@@ -13,14 +13,14 @@ export interface BookFormProps {
   }
   
 
-  interface BookListProps {
+  export interface BookListProps {
     books: Book[]; // Array of books to display
     onDeleteBook: (id: string) => void; // Function to delete a book
     onViewBookDetails?: (book: Book) => void; // Optional: View book details
     onEditBook?: (book: Book) => void; // Optional: Edit a book
   }
   
-interface BookCardProps {
+export interface BookCardProps {
     book: Book; // Book data to display
     onDelete: (id: string) => void; // Function to delete the book
     onViewDetails?: (book: Book) => void; // Optional: Function to view more details
